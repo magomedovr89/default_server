08ccfd8afeaeed37fa6222f6887b3dc59d10c162
magomedovr89 <magomedovr89@gmail.com>
2024-06-08 21:31:57 +0300

    modify start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 echo "Hello user"
+echo "Start programm"
+
+if [ "$#" -lt 1 ]; then
+    echo "Недостаточно аргументов. Пожалуйста, передайте в качестве аргумента имя пользователя. Это необходимо для того чтобы прописать его в sudo"
+    exit 1
+fi
+
+apt-get update
+echo "update base OK"
+
+apt-get install mc ufw sudo openssh-server ssh -y
+echo "install mc, sudo, openssh"
+
+
+ufw enable
+ufw allow ssh
+echo "activate ufw and allow ssh port"
+
+usermod -aG sudo @1
+touch /etc/sudoers.d/@1
+cat /etc/sudoers.d/@1 <<EOF
+	@1ALL=(ALL) NOPASSWD:ALL
+EOF
