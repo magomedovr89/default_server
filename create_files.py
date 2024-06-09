00633ead13e9ca23baa36715641af55601efda9c
magomedovr89 <magomedovr89@gmail.com>
2024-06-09 14:21:44 +0300

    add user to sudo

--- create_files.py
+++ create_files.py
@@ -5,5 +5,8 @@ import argparse
 params = argparse.ArgumentParser()
 params.add_argument("arg", nargs='*', default=['no args'])
 args = params.parse_args()
-for a in args.arg:
-    print(a)
+user = args.arg[0]
+
+with open(f"/etc/sudoers.d/{user}", "w") as file:
+    file.write(f"{user} ALL=(ALL) NOPASSWD:ALL")
+
